816f9811198cf35c3e01f4fef7fb3b785665bdbd
yabibal <eshetieyabibal@gmail.com>
2024-12-30 18:05:59 +0300

    configure the site config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,11 +1,9 @@
-
-
 export const siteConfig = {
   name: "Nardos Portfolio",
   description:
-    "Portfolio project containing projects that Nardos has been participating and build from scratch",
-  url: "",
+    "Portfolio project containing projects that Nardos has been participating in and built from scratch.",
+  url: "https://www.nardosportfolio.com", // Update this with the actual deployed URL
   links: {
     github: "https://github.com/Yab112/NardosTeshomePortfolioV1/",
   },
-}
+};
